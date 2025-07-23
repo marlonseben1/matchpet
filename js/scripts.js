5627519b1d3ed47392b54290c2043f29f56e8f24
Marlon <marlonseben1@gmail.com>
2025-07-22 21:04:09 -0300

    Fix: Fixed import _reset.scss problem | Added: Better animations and font

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-// Waits until the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   const checkbox = document.getElementById("checkbox");
   const navigation = document.querySelector(".navigation");
 
-  function toggleNavigation() {
-    navigation.classList.toggle("hidden");
-  }
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      // Remove a animação de saída e aplica a de entrada
+      navigation.classList.remove("navigation--close");
+      navigation.classList.add("navigation--open");
+    } else {
+      // Remove a animação de entrada e aplica a de saída
+      navigation.classList.remove("navigation--open");
+      navigation.classList.add("navigation--close");
 
-  checkbox.addEventListener("change", toggleNavigation);
+      // Após a animação de saída, desativa pointer-events
+      setTimeout(() => {
+        navigation.classList.remove("navigation--close");
+      }, 450); // Duração da animação
+    }
+  });
 });
